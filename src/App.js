267deeee0b71763daad6f065e17fc4f2ce4ef838
LukeMocroski <devmocroski@gmail.com>
2023-01-30 09:53:27 -0300

    Fazendo requisicao para api e obtendo produtos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
 
+  //usestate
 const [btnCadastrar, setBtnCadastrar] = useState(true);
+const [produtos, setProdutos] = useState([]);
+
+//useEffect hook executado quando componetne é montado
+useEffect(() =>{
+
+  fetch("http://localhost:8080/listar")
+  .then(retorno => retorno.json())
+  .then(retorno_convertido => setProdutos(retorno_convertido));
+
+}, []);
 
 
   return (
     <div >
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar}/>
       <Tabela/>
     </div>

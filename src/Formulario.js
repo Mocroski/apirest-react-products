cfef15f12ac9d77a895c7ebcd4abf7d1aa72b30d
LukeMocroski <devmocroski@gmail.com>
2023-01-30 08:42:35 -0300

    Criando e estruturando componentes

---
+++ src/Formulario.js
@@ -0,0 +1,16 @@
+function Formulario(){
+
+    return(
+        <form>
+            <input type="text" placeholder="Nome" />
+            <input type="text" placeholder="Marca" />
+
+            <input type='button' value='Cadastrar'/>
+            <input type='button' value='Alterar'/>
+            <input type='button' value='Remover'/>
+            <input type='button' value='Cancelar'/>
+        </form>
+    )
+}
+
+export default Formulario;
